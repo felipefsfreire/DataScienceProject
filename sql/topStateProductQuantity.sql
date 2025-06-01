9d3702fe195196d9523efa51aa8c52e51939c4bd
felipefsfreire <felipe.freire1983@gmail.com>
2025-06-01 10:12:01 -0300

    Create query for Top N Products by State

--- sql/topStateProductQuantity.sql
+++ sql/topStateProductQuantity.sql
@@ -8,7 +8,31 @@
 -- Se houver empate na quantidade vendida, desempate pelo produto em ordem alfabética.
 ---------------------------------------------------------------------------------------
 
-WITH topCityProductQuantity AS (
+
+WITH extractingState AS (
     SELECT
-    
+        TRIM(SUBSTR(address, INSTR(address, ' /') + 2)) AS state,
+        customer_id
+    FROM customers
+),
+
+topStateProductQuantity AS (
+    SELECT
+        es.state,
+        p.product_name AS product,
+        SUM(oi.order_quantity) AS total_quantity,
+        rank() OVER (PARTITION BY es.state ORDER BY SUM(oi.order_quantity) DESC, p.product_name ASC) AS rank
+    FROM order_items oi
+    JOIN orders o ON oi.order_id = o.order_id
+    JOIN products p ON oi.product_id = p.product_id
+    JOIN extractingState es ON o.customer_id = es.customer_id
+    GROUP BY es.state, p.product_name
 )
+
+SELECT
+    state,
+    product,
+    total_quantity,
+    rank
+FROM topStateProductQuantity
+WHERE rank <= 3
